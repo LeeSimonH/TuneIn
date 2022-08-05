525dbd5739bec5995f25402a814d544511b550cb
Simon <simonlee1125@gmail.com>
2022-08-05 14:50:18 -0400

    Track reviews now display as an overlay on the page and don't alter layout of the tracks 'behind' them.

--- src/components/Track.js
+++ src/components/Track.js
@@ -30,13 +30,19 @@ const Track = (props) => {
 
   function TrackReview({ rating, review }) {
     return (
-      <div className="review">
-        <ul>
-          <li className="track-rating">
-            Rating: {rating ? rating + ' / 5 stars' : 'N/A'}
-          </li>
-          <li className="review-text">Review: {review ? review : 'N/A'}</li>
-        </ul>
+      <div className="review-container">
+        <div className="review">
+          <ul>
+            <li className="track-rating">
+              Rating: {rating ? rating + ' / 5 stars' : 'N/A'}
+            </li>
+            <li className="review-text">Review: {review ? review : 'N/A'}</li>
+          </ul>
+        </div>
+        <div className="review-background"></div>
+        <button onClick={handleClick} className="close-review-btn">
+          x
+        </button>
       </div>
     );
   }
